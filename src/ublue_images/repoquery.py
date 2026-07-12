3b8a0bdc0e6f68e92113e20f9c1c468f1754eee5
purkkis <joona@purkamo.me>
2026-07-12 11:58:55 +0300

    queryformat

--- src/ublue_images/repoquery.py
+++ src/ublue_images/repoquery.py
@@ -5,8 +5,8 @@ import subprocess as sp
 def latest_installed_versions(packages: list[str]):
     versions = {}
     for package in packages:
-        qf = '{"%{name}": "%{version}"}\n'
-        cmd = f"dnf repoquery --installed --qf '{qf}' {package}"
+        qf = '{"%{name}": "%{version}"}\n'  # return '{"name": "version"}'
+        cmd = f"dnf repoquery --installed --queryformat '{qf}' {package}"
         result = sp.run(cmd, shell=True, capture_output=True, text=True)
         if not result.stdout.strip():
             versions[package] = None
